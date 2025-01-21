993215838b6c6893a127c80ac26990430887ed3a
Amina ECH-CHAKFY <117484543+Amina-ECH-CHAKFY@users.noreply.github.com>
2025-01-21 18:04:07 +0100

    Initial commit

---
+++ resources/js/Pages/Admin/Components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+      <!-- Your footer content here -->
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+  };
+  </script>
+  
+  <style scoped>
+  /* Your styles here */
+  </style>
